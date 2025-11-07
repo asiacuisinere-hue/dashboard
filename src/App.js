04173e90b2dbe7e8c6e879b2e419654800d3b844
Luxilo <asiacuisine.re@gmail.com>
2025-11-07 09:35:28 +0400

    Fix(build): suppression importation inutilisée

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { supabase } from './supabaseClient';
 import Sidebar from './Sidebar';
 import Scanner from './Scanner';
