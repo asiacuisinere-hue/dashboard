776e7aa1d9a49e97871d053a6057bc1cbd91eee6
Luxilo <asiacuisine.re@gmail.com>
2025-11-23 16:08:11 +0400

    Fix: débogage de la génération de facture

--- src/pages/Devis.js
+++ src/pages/Devis.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 import { supabase } from '../supabaseClient';
 
 const Devis = () => {
